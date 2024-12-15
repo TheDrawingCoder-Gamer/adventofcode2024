09f96b371f414b6af7f3ff495c299e4941aaafdc
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-12-15 14:02:20 -0500

    day 15

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -7,97 +7,102 @@ import javax.imageio.ImageIO
 import scala.io.Source
 import scala.collection.mutable as mut
 
-case class GridSize(x: Int, y: Int)
 
-case class Robot(pos: Vec2i, velocity: Vec2i) {
-  def stepN(n: Int = 1)(using size: GridSize): Robot = {
-    copy(pos = pos.copy(x = (pos.x + n * velocity.x) rem size.x, y = (pos.y + n * velocity.y) rem size.y))
-  }
-}
 
-extension (robots: List[Robot])(using size: GridSize) {
-  def hasRect: Boolean = {
-    val grid = robots.asGrid
+object Day14 extends Problem[List[Day14.Robot], Int] {
+  case class GridSize(x: Int, y: Int)
 
-    grid.zipWithIndices.filterNot(_._1 == 0).exists { (_, p) =>
-      val rightPos = Iterator.iterate(p)(_.offset(Direction2D.Right)).takeWhile(p1 => grid.get(p1.x, p1.y).exists(_ > 0)).toList.last
-      val bottomPos = Iterator.iterate(p)(_.offset(Direction2D.Down)).takeWhile(p1 => grid.get(p1.x, p1.y).exists(_ > 0)).toList.last
-
-      if (p != rightPos && p != bottomPos && grid.isDefinedAt(rightPos.x, rightPos.y) && grid.isDefinedAt(bottomPos.x, bottomPos.y)) {
-        assert(grid(rightPos.x, rightPos.y) > 0)
-        assert(grid(bottomPos.x, bottomPos.y) > 0)
-
-        val bottomRight = Vec2i(rightPos.x, bottomPos.y)
-        val sizeX = rightPos.x - p.x
-        val sizeY = rightPos.y - p.y
-
-        sizeX > 5 &&
-          sizeY > 5 &&
-          grid(rightPos.x, bottomPos.y) > 0 &&
-          p.straightLine(rightPos).forall(it => grid(it.x, it.y) > 0) &&
-          p.straightLine(bottomPos).forall(it => grid(it.x, it.y) > 0) &&
-          rightPos.straightLine(bottomRight).forall(it => grid(it.x, it.y) > 0) &&
-          bottomPos.straightLine(bottomRight).forall(it => grid(it.x, it.y) > 0)
-      } else false
+  case class Robot(pos: Vec2i, velocity: Vec2i) {
+    def stepN(n: Int = 1)(using size: GridSize): Robot = {
+      copy(pos = pos.copy(x = (pos.x + n * velocity.x) rem size.x, y = (pos.y + n * velocity.y) rem size.y))
     }
   }
-  def stepN(n: Int = 1): List[Robot] = robots.map(_.stepN(n))
-  def safety: Int = {
-    val middleX = (size.x / 2)
-    val middleY = (size.y / 2)
 
-    robots.groupBy { robot =>
-      if (robot.pos.x == middleX || robot.pos.y == middleY) {
-        -1
-      } else if (robot.pos.x < middleX) {
-        if (robot.pos.y < middleY) {
-          0
-        } else {
-          2
-        }
-      } else {
-        if (robot.pos.y < middleY) {
-          1
-        } else {
-          3
-        }
+  extension (robots: List[Robot])(using size: GridSize) {
+    def hasRect: Boolean = {
+      val grid = robots.asGrid
+
+      grid.zipWithIndices.filterNot(_._1 == 0).exists { (_, p) =>
+        val rightPos = Iterator.iterate(p)(_.offset(Direction2D.Right))
+                               .takeWhile(p1 => grid.get(p1.x, p1.y).exists(_ > 0)).toList.last
+        val bottomPos = Iterator.iterate(p)(_.offset(Direction2D.Down))
+                                .takeWhile(p1 => grid.get(p1.x, p1.y).exists(_ > 0)).toList.last
+
+        if (p != rightPos && p != bottomPos && grid.isDefinedAt(rightPos.x, rightPos.y) && grid
+          .isDefinedAt(bottomPos.x, bottomPos.y)) {
+          assert(grid(rightPos.x, rightPos.y) > 0)
+          assert(grid(bottomPos.x, bottomPos.y) > 0)
+
+          val bottomRight = Vec2i(rightPos.x, bottomPos.y)
+          val sizeX = rightPos.x - p.x
+          val sizeY = rightPos.y - p.y
+
+          sizeX > 5 &&
+            sizeY > 5 &&
+            grid(rightPos.x, bottomPos.y) > 0 &&
+            p.straightLine(rightPos).forall(it => grid(it.x, it.y) > 0) &&
+            p.straightLine(bottomPos).forall(it => grid(it.x, it.y) > 0) &&
+            rightPos.straightLine(bottomRight).forall(it => grid(it.x, it.y) > 0) &&
+            bottomPos.straightLine(bottomRight).forall(it => grid(it.x, it.y) > 0)
+        } else false
       }
-    }.removed(-1).values.map(_.length).product
-  }
-  def pretty: String = {
-    val grid = robots.robotMap
-
-    grid.map(_.map(prettyCharForNum.andThen { case '0' => '.' case i => i }).mkString("", "", "")).mkString("", "\n", "")
-  }
-  def robotMap: Vector[Vector[Int]] = {
-    val goodGrid = mut.ArrayBuffer.fill(size.y, size.x)(0)
-
-    robots.foreach { robot =>
-      goodGrid(robot.pos.y)(robot.pos.x) = goodGrid(robot.pos.y)(robot.pos.x) + 1
     }
+    def stepN(n: Int = 1): List[Robot] = robots.map(_.stepN(n))
+    def safety: Int = {
+      val middleX = (size.x / 2)
+      val middleY = (size.y / 2)
 
-    goodGrid.map(_.toVector).toVector
+      robots.groupBy { robot =>
+        if (robot.pos.x == middleX || robot.pos.y == middleY) {
+          -1
+        } else if (robot.pos.x < middleX) {
+          if (robot.pos.y < middleY) {
+            0
+          } else {
+            2
+          }
+        } else {
+          if (robot.pos.y < middleY) {
+            1
+          } else {
+            3
+          }
+        }
+      }.removed(-1).values.map(_.length).product
+    }
+    def pretty: String = {
+      val grid = robots.robotMap
+
+      grid.map(_.map(prettyCharForNum.andThen { case '0' => '.' case i => i }).mkString("", "", ""))
+          .mkString("", "\n", "")
+    }
+    def robotMap: Vector[Vector[Int]] = {
+      val goodGrid = mut.ArrayBuffer.fill(size.y, size.x)(0)
+
+      robots.foreach { robot =>
+        goodGrid(robot.pos.y)(robot.pos.x) = goodGrid(robot.pos.y)(robot.pos.x) + 1
+      }
+
+      goodGrid.map(_.toVector).toVector
+    }
+    def asGrid: Grid[Int] = Grid(robotMap)
+
+    def asImage: BufferedImage =
+      val image = BufferedImage(size.x, size.y, BufferedImage.TYPE_INT_RGB)
+      val g = image.createGraphics()
+      val rset = robots.map(_.pos).toSet
+      for
+        x <- 0 until size.x
+        y <- 0 until size.y
+      do
+        g.setColor(
+          if rset(Vec2i(x, y)) then java.awt.Color.BLACK else java.awt.Color.WHITE,
+        )
+        g.fillRect(x, y, 1, 1)
+      g.dispose()
+
+      image
   }
-  def asGrid: Grid[Int] = Grid(robotMap)
-
-  def asImage: BufferedImage =
-    val image = BufferedImage(size.x, size.y, BufferedImage.TYPE_INT_RGB)
-    val g = image.createGraphics()
-    val rset = robots.map(_.pos).toSet
-    for
-      x <- 0 until size.x
-      y <- 0 until size.y
-    do
-      g.setColor(
-        if rset(Vec2i(x, y)) then java.awt.Color.BLACK else java.awt.Color.WHITE,
-      )
-      g.fillRect(x, y, 1, 1)
-    g.dispose()
-
-    image
-}
-
-object Day14 extends Problem[List[Robot], Int] {
   override def parse(str: String): List[Robot] = {
     str.linesIterator.map { case s"p=$px,$py v=$vx,$vy" => Robot(Vec2i(px.toInt, py.toInt), Vec2i(vx.toInt, vy.toInt)) }.toList
   }
@@ -123,9 +128,11 @@ object Day14 extends Problem[List[Robot], Int] {
 
   lazy val input: String = Source.fromResource("day14.txt").mkString.trim
 }
-
+/*
 @main def main(): Unit = {
   println(-2 rem 6)
   println(Day14.fullPart1)
   println(Day14.fullPart2)
 }
+
+ */
---
+++ src/main/scala/Day15.scala
@@ -0,0 +1,195 @@
+import gay.menkissing.advent.Problem
+import gay.menkissing.common.{Direction2D, *}
+import cats.Eval
+import cats.syntax.all.*
+
+import scala.annotation.{tailrec, targetName}
+import scala.io.Source
+import scala.collection.mutable as mut
+import GridAxisSystem.*
+
+
+
+object Day15 extends Problem[Day15.ProblemState, Long] {
+  enum GridItem:
+    case Empty, Wall, Box
+
+  enum GridItemP2:
+    case Empty, Wall, BoxL, BoxR
+
+  extension (grid: Grid[GridItem]) {
+    @targetName("updatedMoveP1")
+    def updatedMove(start: Vec2i, dir: Direction2D): Option[Grid[GridItem]] = {
+      @tailrec
+      def go(curGrid: Grid[GridItem], start: Vec2i): Option[Grid[GridItem]] =
+        val newPos = start.genOffset(dir)
+        val oldItem = curGrid(start)
+        curGrid(newPos) match {
+          case GridItem.Wall => None
+          case GridItem.Empty => Some(curGrid.updated(newPos)(oldItem))
+          case GridItem.Box => go(curGrid, newPos)
+        }
+
+      // prevent weird
+      go(grid.updated(start)(GridItem.Empty), start).map: it =>
+        val newPos = start.genOffset(dir)
+        if it.isDefinedAt(newPos.x, newPos.y) then
+          it.updated(newPos)(GridItem.Empty)
+        else it
+    }
+    def pretty: String =
+      (grid.values.map: it =>
+        (it.map:
+          case GridItem.Empty => '.'
+          case GridItem.Wall => '#'
+          case GridItem.Box => 'O').mkString("", "", "")).mkString("", "\n", "")
+  }
+
+  extension (grid: Grid[GridItemP2])
+    @targetName("updatedMoveP2")
+    def updatedMove(istart: Vec2i, dir: Direction2D): Option[Grid[GridItemP2]] =
+      // ???
+      def go(start: Vec2i, movedItems: List[(Vec2i, GridItemP2)]): Eval[Option[List[(Vec2i, GridItemP2)]]] =
+        val newPos = start.genOffset(dir)
+        grid(newPos) match
+          case GridItemP2.Wall => Eval.always(None)
+          case GridItemP2.Empty => Eval.now(Some(movedItems))
+          case GridItemP2.BoxL =>
+            val rPos = newPos.genOffset(Direction2D.Right)
+            assert(grid(rPos) == GridItemP2.BoxR)
+            val newItems = movedItems.prepended(newPos, GridItemP2.BoxL)
+            dir match
+              case Direction2D.Left | Direction2D.Right =>
+                go(newPos, newItems)
+              case Direction2D.Up | Direction2D.Down =>
+                go(newPos, newItems).flatMap:
+                  case Some(it) => go(rPos, it.prepended(rPos, GridItemP2.BoxR))
+                  case None => Eval.now(None)
+          case GridItemP2.BoxR =>
+            val lPos = newPos.genOffset(Direction2D.Left)
+            assert(grid(lPos) == GridItemP2.BoxL)
+            val newItems = movedItems.prepended(newPos, GridItemP2.BoxR)
+            dir match
+              case Direction2D.Right | Direction2D.Left =>
+                go(newPos, newItems)
+              case Direction2D.Up | Direction2D.Down =>
+                go(newPos, newItems).flatMap:
+                  case Some(it) => go(lPos, it.prepended(lPos, GridItemP2.BoxL))
+                  case None => Eval.now(None)
+
+
+      go(istart, List()).value.map:
+        val alreadyInspected = mut.Set[Vec2i]()
+        _.foldLeft(grid.updated(istart)(GridItemP2.Empty)):
+          case (g, (p, item)) =>
+            if (!alreadyInspected.contains(p))
+              alreadyInspected.add(p)
+              g.updated(p)(GridItemP2.Empty).updated(p.genOffset(dir))(item)
+            else g
+
+  case class ProblemState(grid: Grid[GridItem], robot: Vec2i, remainingMoves: List[Direction2D]):
+    def step: Option[ProblemState] =
+      remainingMoves match
+        case head :: rest =>
+          grid.updatedMove(robot, head) match
+            case Some(newGrid) => Some(ProblemState(newGrid, robot.genOffset(head), rest))
+            case _ => Some(ProblemState(grid, robot, rest))
+        case _ => None
+
+    def gpsCalc: Long =
+      grid.zipWithIndices.filter(_._1 == GridItem.Box).map((_, p) => p.y.toLong * 100L + p.x.toLong).sum
+
+    def doubled: ProblemStateP2 =
+      val newGrid = Grid(grid.values.map(_.flatMap:
+        case GridItem.Box => List(GridItemP2.BoxL, GridItemP2.BoxR)
+        case GridItem.Wall => List(GridItemP2.Wall, GridItemP2.Wall)
+        case GridItem.Empty => List(GridItemP2.Empty, GridItemP2.Empty)
+      ))
+      ProblemStateP2(newGrid, robot.copy(x = robot.x * 2), remainingMoves)
+    def pretty: String =
+      (grid.values.zipWithIndex.map: (it, y) =>
+        (it.zipWithIndex.map: (m, x) =>
+          m match
+            case _ if Vec2i(x, y) == robot => '@'
+            case GridItem.Empty => '.'
+            case GridItem.Wall => '#'
+            case GridItem.Box => 'O').mkString("", "", "")).mkString("", "\n", "")
+
+  case class ProblemStateP2(grid: Grid[GridItemP2], robot: Vec2i, remainingMoves: List[Direction2D]):
+    def step: Option[ProblemStateP2] =
+      remainingMoves match
+        case head :: rest =>
+          grid.updatedMove(robot, head) match
+            case Some(newGrid) => Some(ProblemStateP2(newGrid, robot.genOffset(head), rest))
+            case _ => Some(ProblemStateP2(grid, robot, rest))
+        case _ => None
+
+    def gpsCalc: Long =
+      grid.zipWithIndices.filter(_._1 == GridItemP2.BoxL).map((_, p) => p.y.toLong * 100L + p.x.toLong).sum
+
+    def pretty: String =
+      (grid.values.zipWithIndex.map: (it, y) =>
+        (it.zipWithIndex.map: (m, x) =>
+          m match
+            case _ if Vec2i(x, y) == robot => '@'
+            case GridItemP2.Empty => '.'
+            case GridItemP2.Wall => '#'
+            case GridItemP2.BoxL => '['
+            case GridItemP2.BoxR => ']').mkString("", "", "")).mkString("", "\n", "")
+  override def parse(str: String): ProblemState =
+    val sides = str.split("\n\n")
+    var robotPos = Vec2i(-1, -1)
+    val grid = Grid(sides(0).linesIterator.zipWithIndex.map: (str, y) =>
+      str.toVector.zipWithIndex.map: (it, x) =>
+        it match
+          case '#' => GridItem.Wall
+          case 'O' => GridItem.Box
+          case '.' => GridItem.Empty
+          case '@' =>
+            assert(robotPos == Vec2i(-1, -1))
+            robotPos = Vec2i(x, y)
+            GridItem.Empty
+          case _ => ???
+      )
+    val dirs = sides(1).linesIterator.flatten.map:
+      case '<' => Direction2D.Left
+      case '^' => Direction2D.Up
+      case '>' => Direction2D.Right
+      case 'v' => Direction2D.Down
+      case _ => ???
+
+    ProblemState(grid, robotPos, dirs.toList)
+
+  override def part1(input: ProblemState): Long =
+    @tailrec
+    def go(i: ProblemState): ProblemState =
+      if i.remainingMoves.nonEmpty then
+        go(i.step.get)
+      else i
+
+    //println(input.pretty)
+    val res = go(input)
+    //println(res.grid.pretty)
+    res.gpsCalc
+
+  override def part2(input: ProblemState): Long =
+    @tailrec def go(i: ProblemStateP2): ProblemStateP2 =
+      if i.remainingMoves.nonEmpty then
+        val r = i.step.get
+        //println(i.remainingMoves.head)
+        //println(r.pretty)
+        go(r)
+      else i
+
+    // println(input.doubled.step.get.step.get.pretty)
+    val res = go(input.doubled)
+    res.gpsCalc
+
+  override def input: String = Source.fromResource("day15.txt").mkString
+}
+
+@main def main(): Unit =
+  println(Day15.fullPart1)
+  println(Day15.fullPart2)
+
+
