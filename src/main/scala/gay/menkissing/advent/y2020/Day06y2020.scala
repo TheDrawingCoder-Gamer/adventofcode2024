d812813648d5de8a1670d0c1e0fae2e5268aba62
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-12-29 15:34:49 -0500

    day 6 2020

---
+++ src/main/scala/gay/menkissing/advent/y2020/Day06y2020.scala
@@ -0,0 +1,20 @@
+package gay.menkissing.advent
+package y2020
+
+object Day06y2020 extends Problem[List[List[Set[Char]]], Int]:
+  override def parse(str: String): List[List[Set[Char]]] =
+    str.split("\n\n").map: block =>
+      block.linesIterator.map(_.toSet).toList
+    .toList
+
+  override def part1(input: List[List[Set[Char]]]): Int =
+    input.map: block =>
+      block.reduce(_ | _).size
+    .sum
+
+  override def part2(input: List[List[Set[Char]]]): Int =
+    input.map: block =>
+      block.reduce(_ & _).size
+    .sum
+
+  override val input: String = FileIO.getInput(2020, 6)
