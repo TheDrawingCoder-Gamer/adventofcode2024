5f48dcec595efcafcfca2e1da80c3d113abe5729
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-07-16 21:49:59 -0400

    day 11 2015

---
+++ core/shared/src/main/scala/gay/menkissing/advent/y2015/Day11.scala
@@ -0,0 +1,48 @@
+package gay.menkissing.advent
+package y2015
+
+import cats.*
+import cats.syntax.all.*
+
+object Day11 extends Problem[String, String]:
+  def parse(str: String): String = str.trim
+
+  def test(input: String): Boolean =
+    val ls = input.toList.zipWithIndex.sliding2.flatMap:
+      case ((a, i), (b, j)) =>
+        Option.when(a == b)(a.toString + b.toString, List(i, j))
+    input.forall {
+      case 'i' | 'o' | 'l' => false
+      case _ => true
+    } && ls.exists((a, i) => ls.exists((b, j) => a != b && i.intersect(j).isEmpty))
+      && input.toList.sliding3.exists: (a, b, c) =>
+        c - b == 1 && b - a == 1
+  def advance(input: String): String =
+    val arr = input.getBytes("US-ASCII")
+    var i = arr.length - 1
+    while i >= 0 do
+      arr(i) = (arr(i) + 1).toByte
+      if arr(i) > 'z' then
+        arr(i) = 'a'
+        i -= 1
+      else
+        arr(i).toChar match
+          case 'i' =>
+            arr(i) = 'j'
+          case 'o' =>
+            arr(i) = 'p'
+          case 'l' =>
+            arr(i) = 'm'
+          case _ => ()
+        i = -1
+
+
+    new String(arr, "US-ASCII")
+
+  def part1(input: String): String =
+    Iterator.iterate(advance(input))(advance).find(test).get
+
+  def part2(input: String): String =
+    Iterator.iterate(advance(input))(advance).dropWhile(it => !test(it)).drop(1).find(test).get
+
+  lazy val input: String = FileIO.getInput(2015, 11)
