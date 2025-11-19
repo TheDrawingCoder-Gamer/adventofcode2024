98ef6df8ef21f27b49fb1fabe969a80e6f9fcf2c
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-11-18 21:47:05 -0500

    fix broken day 11 2015?

--- core/shared/src/main/scala/gay/menkissing/advent/y2015/Day11.scala
+++ core/shared/src/main/scala/gay/menkissing/advent/y2015/Day11.scala
@@ -1,47 +1,59 @@
 package gay.menkissing.advent
 package y2015
 
-import cats.*
-import cats.syntax.all.*
-
 object Day11 extends Problem:
   type Input = String
   type Output = String
 
   def parse(str: String): String = str.trim
 
-  def test(input: String): Boolean =
-    val ls =
-      input.toList.zipWithIndex.sliding2.flatMap:
-        case ((a, i), (b, j)) =>
-          Option.when(a == b)(a.toString + b.toString, List(i, j))
-    input.forall("iol".contains) && ls.zip(ls).exists:
-      case ((a, i), (b, j)) => a != b && i.intersect(j).isEmpty
-    && input.toList.sliding3.exists: (a, b, c) =>
-      c - b == 1 && b - a == 1
-  def advance(input: String): String =
-    val arr = input.getBytes("US-ASCII")
+  val iolBytes = "iol".getBytes("US-ASCII").map(it => (it - 'a').toByte)
+  val shortenedAlpha = "abcdefghjkmnpqrstuvwxyz".getBytes("US-ASCII")
+
+  def test(input: Array[Byte]): Boolean =
+
+    !input.exists(iolBytes.contains) && locally:
+      val ls =
+        input.zipWithIndex.sliding(2).flatMap:
+          case Array((a, i), (b, j)) => Option.when(a == b)(a, List(i, j))
+        .toArray
+      ls.flatMap(x => ls.map(y => (x, y))).exists:
+        case ((a, i), (b, j)) => a != b && i.intersect(j).isEmpty
+    && input.sliding(3).exists:
+      case Array(a, b, c) => c - b == 1 && b - a == 1
+
+  def advance(arr: Array[Byte]): Array[Byte] =
     var i = arr.length - 1
     while i >= 0 do
       arr(i) = (arr(i) + 1).toByte
-      if arr(i) > 'z' then
-        arr(i) = 'a'
+      if arr(i) > 25 then
+        arr(i) = 0
         i -= 1
       else
+        /*
         arr(i).toChar match
           case 'i' => arr(i) = 'j'
           case 'o' => arr(i) = 'p'
           case 'l' => arr(i) = 'm'
           case _   => ()
+         */
         i = -1
 
-    new String(arr, "US-ASCII")
+    arr
 
+  def toBytes(str: String): Array[Byte] =
+    str.map(it => (it - 'a').toByte).toArray
+  def fromBytes(bytes: Array[Byte]): String =
+    new String(bytes.map(it => (it + 'a').toByte), "US-ASCII")
   def part1(input: String): String =
-    Iterator.iterate(advance(input))(advance).find(test).get
+    val bytes =
+      Iterator.iterate(advance(toBytes(input)))(advance).find(test).get
+    fromBytes(bytes)
 
   def part2(input: String): String =
-    Iterator.iterate(advance(input))(advance).dropWhile(it => !test(it)).drop(1)
-      .find(test).get
+    val bytes =
+      Iterator.iterate(advance(toBytes(input)))(advance)
+        .dropWhile(it => !test(it)).drop(1).find(test).get
+    fromBytes(bytes)
 
   def input: String = FileIO.getInput(2015, 11)
