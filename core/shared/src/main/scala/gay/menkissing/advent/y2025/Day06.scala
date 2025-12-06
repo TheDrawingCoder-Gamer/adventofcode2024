02f203be69a8d31b48c5c6080e1642c4df51cbe6
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-12-06 12:44:49 -0500

    fixup day 6

--- core/shared/src/main/scala/gay/menkissing/advent/y2025/Day06.scala
+++ core/shared/src/main/scala/gay/menkissing/advent/y2025/Day06.scala
@@ -14,27 +14,21 @@ object Day06 extends ProblemUniqueInputs:
   def input: String = FileIO.getInput(2025, 6)
 
   def parseP1(str: String): List[SquidProblem] =
-    val arr = str.linesIterator.toArray
-    val terms =
-      arr.init.map(_.trim.split(raw"\s+").map(_.toLong).toList).toList.transpose
-    val operators = arr.last.trim.split(raw"\s+").map(_.head)
-    terms.zip(operators).map: (terms, operator) =>
-      (terms = terms, operator = operator)
-    .toList
+    str.linesIterator.toList.map(_.split(raw"\s+").toList).transpose.map:
+      problem =>
+        (terms = problem.init.map(_.toLong), operator = problem.last.head)
 
   def parseP2(str: String): List[SquidProblem] =
-    val arr = str.linesIterator.toArray
+    val ls = str.linesIterator.toList
     val termColumns =
-      arr.init.map:
+      ls.init.map:
         _.map(it => Option.unless(it.isSpaceChar)(it.asDigit)).toList
-      .toList.transpose.map: it =>
-        val flattened = it.flatten
-        // if our entire thing is empty then we got a blank column
-        if flattened.isEmpty then -1
-        // otherwise we have a number
-        else flattened.foldLeft(0)((acc, n) => acc * 10 + n)
+      .transpose.map:
+        _.flatten match
+          case Nil => -1
+          case vs  => vs.foldLeft(0)((acc, n) => acc * 10 + n)
     val groupedTerms = termColumns.splitOn(-1)
-    val operators = arr.last.trim.split(raw"\s+").map(_.head)
+    val operators = ls.last.trim.split(raw"\s+").map(_.head)
     groupedTerms.zip(operators).map: (terms, operator) =>
       (terms = terms.map(_.toLong), operator = operator)
 
